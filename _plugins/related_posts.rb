c9b727d756c9a6ff05e9177eee4bf040a86748f7
TheDigitalPortrait <jantana2602@gmail.com>
2015-05-16 20:11:40 +0700

    cc

--- _plugins/related_posts.rb
+++ _plugins/related_posts.rb
@@ -1,66 +1,40 @@
-require 'jekyll/post'
-
-module RelatedPosts
-
-  # Used to remove #related_posts so that it can be overridden
-  def self.included(klass)
-    klass.class_eval do
-      remove_method :related_posts
-    end
-  end
-
-  # Calculate related posts.
-  #
-  # Returns [<Post>]
-  def related_posts(posts)
-    return [] unless posts.size > 1
-    highest_freq = Jekyll::Post.tag_freq(posts).values.max
-    related_scores = Hash.new(0)
-    posts.each do |post|
-      post.tags.each do |tag|
-        if self.tags.include?(tag) && post != self
-          cat_freq = Jekyll::Post.tag_freq(posts)[tag]
-          related_scores[post] += (1+highest_freq-cat_freq)
-        end
-      end
-    end
-
-    Jekyll::Post.sort_related_posts(related_scores)
-  end
-
-  module ClassMethods
-    # Calculate the frequency of each tag.
-    #
-    # Returns {tag => freq, tag => freq, ...}
-    def tag_freq(posts)
-      return @tag_freq if @tag_freq
-      @tag_freq = Hash.new(0)
-      posts.each do |post|
-        post.tags.each {|tag| @tag_freq[tag] += 1}
-      end
-      @tag_freq
-    end
-
-    # Sort the related posts in order of their score and date
-    # and return just the posts
-    def sort_related_posts(related_scores)
-      related_scores.sort do |a,b|
-        if a[1] < b[1]
-          1
-        elsif a[1] > b[1]
-          -1
-        else
-          b[0].date <=> a[0].date
-        end
-      end.collect {|post,freq| post}
-    end
-  end
-
-end
-
 module Jekyll
-  class Post
-    include RelatedPosts
-    extend RelatedPosts::ClassMethods
+  class RelatedBlogPosts < Liquid::Tag
+    def initialize(tag_name, amount, tokens)
+      super
+      @amount = amount.to_i
+    end
+ 
+    def normalize_tags(tags)
+      tags.map do |tag|
+        tag.downcase.gsub(' ', '')
+      end
+    end
+ 
+    def render(context)
+      post = context['page']
+      post_tags = normalize_tags(post['tags'])
+ 
+      other_posts = context['site']['posts'].select do |other_post|
+        other_post['title'] != post['title']
+      end
+ 
+      sorted_posts = other_posts.sort do |other_post|
+        other_tags = normalize_tags(other_post['tags'])
+        (other_tags - post_tags).count
+      end
+ 
+      closest = sorted_posts[0..(@amount - 1)]
+      html = '<ul>'
+      closest.each do |related_post|
+        html << '<li>'
+        html << "<a href=#{related_post['url']}>#{related_post['title']}</a>"
+        html << "<p>#{related_post['excerpt']}</p>"
+        html << '</li>'
+      end
+      html << '</ul>'
+    end
   end
 end
+ 
+Liquid::Template.register_tag('related_posts', Jekyll::RelatedBlogPosts)
